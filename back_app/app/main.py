5040511e29ae5d8f552695d0a818f4a8327dc68d
pashtetik2901 <gordeevk108@gmail.com>
2025-11-01 18:35:10 +0300

    test_host

--- back_app/app/main.py
+++ back_app/app/main.py
@@ -13,7 +13,7 @@ class LinkData(BaseModel):
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5173", "http://localhost:5174"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
